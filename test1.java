7c129b586c928a7cadeb1fba8c33907a6a957d8c
gbcltr <85185163+gbcltr@users.noreply.github.com>
2021-06-01 22:01:07 +0400

    Delete test1.java

--- test1.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        system.out.print("hello world");
-        system.out.print("Welcome to the world!");
-        //this is a comment
-    }
-}
