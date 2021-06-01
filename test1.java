cfe4d06919a09439965c4d1066c6e733d308cc58
sa1github <85185985+sa1github@users.noreply.github.com>
2021-06-01 19:49:57 +0300

    Update test1.java

--- test1.java
+++ test1.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         system.out.print("hello world");
         system.out.print("Welcome to the world!");
+        //this is a comment
     }
 }
